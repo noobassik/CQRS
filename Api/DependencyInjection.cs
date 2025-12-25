163029ca09713343f347be413f2e0c475f25a2cb
David Sandrosian <david.sandrosian@indusoft.ru>
2025-12-25 12:14:11 +0300

    Add react policy

--- Api/DependencyInjection.cs
+++ Api/DependencyInjection.cs
@@ -9,11 +9,24 @@ namespace Api
             services.AddExceptionHandler<CustomExceptionHandler>();
             services.AddControllers();
             services.AddOpenApi();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("react-policy", policy =>
+                {
+                    policy.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .WithOrigins("http://localhost:3000");
+                });
+            });
+
             return services;
         }
 
         public static WebApplication UserApiServices(this WebApplication app) 
         {
+            app.UseCors("react-policy");
+
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
