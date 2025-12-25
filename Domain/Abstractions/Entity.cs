1512e53492452d81fef5e6d3327ce4409e50c24b
David Sandrosian <david.sandrosian@indusoft.ru>
2025-12-25 10:55:39 +0300

    Add soft delete

--- Domain/Abstractions/Entity.cs
+++ Domain/Abstractions/Entity.cs
@@ -3,5 +3,8 @@
     public abstract class Entity<T> : IEntity<T>
     {
         public required T Id { get; set; }
+
+        public bool IsDeleted { get; set; } = default;
+        public DateTimeOffset? DeletedAt { get; set; }
     }
 }
