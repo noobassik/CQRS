43e08194b2a492c3873ed94d49ec640c562cc4ab
David Sandrosyan <david-010803@mail.ru>
2026-01-09 18:14:09 +0300

    Add jwt security service to DI

--- Infrastructure/DependencyInjection.cs
+++ Infrastructure/DependencyInjection.cs
@@ -1,26 +1,31 @@
-﻿using CQRS.Application.Data.DataBaseContext;
+﻿using Application.Authentication.Services;
+using CQRS.Application.Data.DataBaseContext;
 using Infrastructure.Data.DataBaseContext;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure
 {
-    public static class DependencyInjection
-    {
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("PostgresConnection");
+	public static class DependencyInjection
+	{
+		public static IServiceCollection AddInfrastructureServices(
+			this IServiceCollection services,
+			IConfiguration configuration)
+		{
+			var connectionString = configuration.GetConnectionString("PostgresConnection");
 
-            services.AddDbContext<ApplicationDbContext>(options =>
-            {
-                options.UseNpgsql(connectionString);
-            });
+			services.AddDbContext<ApplicationDbContext>(options =>
+			{
+				options.UseNpgsql(connectionString);
+			});
 
-            services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
+			services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
 			AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 
+			services.AddScoped<IJwtSecurityService, JwtSecurityService>();
+
 			return services;
-        }
-    }
+		}
+	}
 }
