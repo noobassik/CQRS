5ac7576f9506bbe295b754f6a302c59727915394
David Sandrosyan <david-010803@mail.ru>
2026-01-09 18:07:51 +0300

    Changed auth controller with commands and queries

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -1,75 +1,36 @@
-﻿using Api.Security.Services;
-using Domain.Security;
-using Domain.Security.Dtos;
+﻿using Application.Authentication.Dtos;
+using Application.Authentication.Queries.LoginUser;
+using Application.Authentication.Queries.RegisterUser;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Api.Controllers
 {
-	[AllowAnonymous]
-	[Route("api/[controller]")]
-	[ApiController]
-	public class AuthController(UserManager<CustomIdentityUser> manager,
-		IJwtSecurityService jwtSecurityService) : ControllerBase
-	{
-		[HttpPost("login")]
-		public async Task<IResult> login(LoginRequest dto)
-		{
-			var user = await manager.FindByEmailAsync(dto.Email);
+	 [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController(IMediator mediator) : ControllerBase
+    {
+        [HttpPost("login")]
+        [ProducesResponseType(typeof(AuthenticationResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> Login([FromBody] LoginUserDto dto)
+        {
+            var query = new LoginUserQuery(dto);
+            var result = await mediator.Send(query);
+            
+            return Ok(new { result = result.Response });
+        }
 
-			if (user is null)
-			{
-				return Results.Unauthorized();
-			}
-
-			var result = await manager.CheckPasswordAsync(user, dto.Password);
-			
-			if (result)
-			{
-				var response = new IdentityUserResponseDto(
-					user.UserName, user.Email, jwtSecurityService.CreateToken(user)
-				);
-				return Results.Ok(new { result = response });
-			}
-
-			return Results.Unauthorized();
-		}
-
-
-		[HttpPost("register")]
-		public async Task<IResult> Register(RegisterUserRequestDto dto)
-		{
-			if (await manager.Users.AnyAsync(u => u.UserName == dto.Username))
-			{
-				return Results.BadRequest("Username занят");
-			}
-			if (await manager.Users.AnyAsync(u => u.Email == dto.Email))
-			{
-				return Results.BadRequest("Email занят");
-			}
-
-			var user = new CustomIdentityUser
-			{
-				FullName = dto.FullName,
-				Email = dto.Email,
-				UserName = dto.Username,
-				About = String.Empty
-			};
-
-			var result = await manager.CreateAsync(user, dto.Password!);
-
-			if (result.Succeeded)
-			{
-				var response = new IdentityUserResponseDto(
-					user.UserName!, user.Email!, jwtSecurityService.CreateToken(user)
-				);
-				return Results.Ok(new { result = response });
-			}
-
-			return Results.BadRequest(result.Errors);
-		}
-	}
+        [HttpPost("register")]
+        [ProducesResponseType(typeof(AuthenticationResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto dto)
+        {
+            var command = new RegisterUserCommand(dto);
+            var result = await mediator.Send(command);
+            
+            return Ok(new { result = result.Response });
+        }
+    }
 }
