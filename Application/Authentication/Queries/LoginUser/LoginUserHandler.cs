3f4a844529e3615694695db4276918c48ef4d805
David Sandrosyan <david-010803@mail.ru>
2026-01-09 17:51:59 +0300

    Add user login query

---
+++ Application/Authentication/Queries/LoginUser/LoginUserHandler.cs
@@ -0,0 +1,44 @@
+﻿using Application.Authentication.Dtos;
+using Application.Authentication.Services;
+using Domain.Security;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Authentication.Queries.LoginUser
+{
+	public class LoginUserHandler(
+		  UserManager<CustomIdentityUser> userManager,
+		  IJwtSecurityService jwtSecurityService)
+		  : IQueryHandler<LoginUserQuery, LoginUserResult>
+	{
+		public async Task<LoginUserResult> Handle(
+			LoginUserQuery request,
+			CancellationToken cancellationToken)
+		{
+			var user = await userManager.FindByEmailAsync(request.LoginDto.Email);
+
+			if (user is null)
+			{
+				throw new UnauthorizedAccessException("Неверный email или пароль");
+			}
+
+			var isPasswordValid = await userManager.CheckPasswordAsync(
+				user,
+				request.LoginDto.Password);
+
+			if (!isPasswordValid)
+			{
+				throw new UnauthorizedAccessException("Неверный email или пароль");
+			}
+
+			var token = jwtSecurityService.CreateToken(user);
+
+			var response = new AuthenticationResponseDto(
+				user.UserName!,
+				user.Email!,
+				token
+			);
+
+			return new LoginUserResult(response);
+		}
+	}
+}
---
+++ Application/Authentication/Queries/LoginUser/LoginUserQuery.cs
@@ -0,0 +1,8 @@
+﻿using Application.Authentication.Dtos;
+
+namespace Application.Authentication.Queries.LoginUser
+{
+	public record LoginUserQuery(LoginUserDto LoginDto) : IQuery<LoginUserResult>;
+
+	public record LoginUserResult(AuthenticationResponseDto Response);
+}
